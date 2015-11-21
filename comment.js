138494ff3a5309c8c16d1d7dff791d6824427d92
郑飞 <fly9i@163.com>
2015-11-21 21:05:30 +0800

    update all

--- comment.js
+++
@@ -1,4 +0,0 @@
-function createPage(){
-  var page = document.createElement('div');
-  div
-}
---
+++ jdou_chrome/content.js
@@ -0,0 +1,3 @@
+$(function(){
+	$("#product-detail .m-tab-trigger").append("<li class='ui-switchable-item trig-item douban-comments'><a href='javascript:;'>豆瓣书评</a></li>")	
+})
